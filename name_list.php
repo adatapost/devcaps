fc9c8c0e105dfa32d19fb72802eb638b869f0175
amychan331@yahoo.com <amychan331@yahoo.com>
2017-03-27 00:27:40 -0700

    created list of names for capping

--- name_list.php
+++ name_list.php
@@ -1,8 +1,7 @@
 <?php
-// List of language
+// List of name
 $language = [
     "coffeescript" => "CoffeeScript",
-    "" => "",
     "ecmascript" => "ECMAScript",
     "html" => "HTML",
     "javascript" => "JavaScript",
@@ -14,35 +13,23 @@ $language = [
     "php" => "PHP",
     "rethinkdb" => "RethinkDB ",
     "sql" => "SQL",
-    "" => "",
     "typescript" => "TypeScript",
-    "" => "",
-    "" => "",
-    "" => "",
     "vb.net" => "VB.NET",
     "xaml" => "XAML",
 ]
 
 $framework = [
     "angularjs" => "AngularJS",
-    "" => "",
     "cakephp" => "CakePHP",
     "django" => "Django",
     "openui5" => "OpenUI5",
-    "" => "",
-    "" => "",
     "ruby on rails" => "Ruby on Rails",
-    "" => "",
 ]
 
 $libary = [
     "a-frame" => "A-Frame",
-    "" => "",
     "jquery" => "jQuery",
-    "" => "",
-    "" => "",
     "numpy" => "NumPy",
-    "" => "",
     "rubygems" => "RubyGems",
     "scipy" => "SciPy",
     "webgl" => "WebGL",
